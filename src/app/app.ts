2f36675348fb9621eecf59b04b6041951413cb36
Carlos Chávez <89101032+CarlosChavez19@users.noreply.github.com>
2026-02-04 17:23:58 -0500

    feat: login component added

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,10 +1,11 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import {NavbarComponent} from './shared/navbar/navbar.component';
+import {LoginComponent} from './features/iam/pages/login.component/login.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, NavbarComponent],
+  imports: [RouterOutlet, NavbarComponent, LoginComponent],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
